b080fedc4050bc21e265842cb1e9bf4159c507a3
Pryce Houck <phouck@ufl.edu>
2026-03-02 13:45:44 -0500

    input formatting for now, will require changes to handle multiple input sets from the same file

--- Formatting.py
+++ Formatting.py
@@ -1,7 +1,28 @@
+"""
+k m
+r1 r2 r3 ... rm
+"""
+sample = """5 10
+1253 120938 1029383 38102938 838428 38102938 838428 1253 120938 1029383
+"""
 
+"""
+WILL REQUIRE UPDATING LATER TO HANDLE MULTIPLE INPUTS IN THE SAME FILE
+"""
 
-def formatter():
-    return None
+def formatter(to_format):
+    cache = []
+    cache_size_i = to_format.find(' ')
+    cache_size = int(to_format[:cache_size_i])
+    for i in range(0, cache_size):
+        cache.append(float('inf'))
+
+    input_size_i = to_format.find('\n')
+    ids = to_format[input_size_i + 1:]
+    input_ids = ids.split()
+
+    return cache, input_ids
 
 if __name__ == "__main__":
-    formatter()
+    ex_cache, ex_ids = formatter(sample)
+    print(f"{ex_cache}\n{ex_ids}")
